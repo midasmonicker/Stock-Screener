dd00ba972468f2b58aa58cd0112b65cb24fbac57
Midasmonicker <michaelonyeweke@yahoo.com>
2026-09-20 02:23:48 +0100

    Add signalService.js

---
+++ src/services/signalService.js
@@ -0,0 +1,13 @@
+// src/services/signalService.js
+const { fetchMarketData } = require('../dataService');
+
+async function getSignals(tickers) {
+  const data = await fetchMarketData(tickers);
+  return data.map(d => ({
+    ...d,
+    signal: null // placeholder until Finviz integration
+  }));
+}
+
+module.exports = { getSignals };
+
