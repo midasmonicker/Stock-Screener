5f8cdd7879f0618e15b39c819cfd9565643a97af
Midasmonicker <michaelonyeweke@yahoo.com>
2026-09-22 00:09:09 +0100

    Update Render endpoints

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const WebSocket = require("ws");
 const cors = require("cors");
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Restrict CORS to your frontend domain
 app.use(cors({
